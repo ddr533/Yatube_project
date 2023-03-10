853fe4fb2fa78f43fde765399ee16764a93264a7
Дрогаль Андрей <ddr533@yandex.ru>
2023-03-10 22:57:23 +0700

    Change name for templatetag

--- yatube/chat/views.py
+++ yatube/chat/views.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 
-from .models import Message
 from posts.models import Group
 from users.models import UserProfile
 from django.utils import timezone
-
 
 
 @login_required
@@ -22,6 +20,5 @@ def get_group_chat(request, group_slug):
     group = get_object_or_404(Group, slug=group_slug)
     messages = group.group_chat.select_related('user').order_by('-date_added')[:20]
     messages = list(reversed(messages))
-    context = {'messages': messages, 'group': group,
-               'time_zone': time_zone,}
+    context = {'messages': messages, 'group': group, 'time_zone': time_zone}
     return render(request, 'chat/group_chat.html', context)
--- yatube/core/templatetags/user_templatetags.py
+++ yatube/core/templatetags/user_templatetags.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
-def update_chat_day(value):
+def update_var(value):
     """Возвращает переданное значение в шаблон.
     Используется в чате для получения номера дня
     текущего сообщения.
